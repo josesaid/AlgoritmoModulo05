2188f46718fd098f74da5eea974f3c69455551fa
Said Olano <josesaid@gmail.com>
2025-12-07 12:54:56 -0600

    Clean up

--- shopping-microservices/order-service/src/main/java/com/example/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.order.repository;
-
-import com.example.order.entity.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
-}
